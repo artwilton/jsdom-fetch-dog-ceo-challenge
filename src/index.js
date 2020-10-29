b45b49e3d4aeb4a15d8b19bff4c40e3133d10b40
artwilton <Arthur.M.Wilton@gmail.com>
2020-10-29 10:04:25 -0400

    almost finished question 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+const dogArray = fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(response => response.json())
+  .then(data => parseArray(data));
+
+
+
+function parseArray(data) {
+    const imageContainer = document.querySelector("#dog-image-container")
+    const img = document.createElement('img')
+    const messageArray = data.message
+    messageArray.forEach(url => {
+        img.innerText = `src="${url}"`
+        imageContainer.append(img)
+    });
+}
