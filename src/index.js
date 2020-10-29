97247d05516cd96024cb2301a027f88762f5354b
artwilton <Arthur.M.Wilton@gmail.com>
2020-10-29 15:35:15 -0400

    challenge 4 completed and refactored

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// CHALLENGE 1 CODE
+// // CHALLENGE 1 CODE
 function renderImage(imageUrl) {
     const img = document.createElement("img")
     img.src = imageUrl
@@ -19,25 +19,65 @@ function renderImage(imageUrl) {
       })
     })
   
-// CHALLENGE 2 and 3 CODE
-function addBreedToUl(breed, breeds) {
-    const li = document.createElement('li')
-    const breedsUl = document.querySelector("#dog-breeds")
-    li.innerText = `${breed}: ${breeds[breed]}`
-    breedsUl.append(li)
-    var notRed = false
-    li.addEventListener('click', function(event) {
-         (notRed == false)  ?
-            event.target.style.color = 'red' : event.target.style.color = 'black' 
-         notRed = !notRed
-         })
-  }
+// CHALLENGE 2 and 3 OLD CODE
+// function addBreedToUl(breed, breeds) {
+//     const li = document.createElement('li')
+//     const breedsUl = document.querySelector("#dog-breeds")
+//     li.innerText = `${breed}: ${breeds[breed]}`
+//     breedsUl.append(li)
+//     var notRed = false
+//     li.addEventListener('click', function(event) {
+//          (notRed == false)  ?
+//             event.target.style.color = 'red' : event.target.style.color = 'black' 
+//          notRed = !notRed
+//          })
+//   }
 
 fetch("https://dog.ceo/api/breeds/list/all")
 .then(response => response.json())
 .then(data => {
     const breeds = data.message
     for (const breed in breeds) {
-        addBreedToUl(breed, breeds);
-      }
+        addBreedToUl(breed, breeds)
+    }
+    sortBreed()
+    turnRed()
+})
+
+// CHALLENGE 4 CODE
+
+const breedDropdown = document.getElementById("breed-dropdown")
+const breedsUl = document.querySelector("#dog-breeds")
+
+function addBreedToUl(breed, breeds) {
+    const li = document.createElement('li')
+    breedsUl.append(li)
+    li.innerText = `${breed}: ${breeds[breed]}`
+    li.style.display = "none"
+}
+
+breedDropdown.addEventListener('change', function(event) {
+    sortBreed(event.target.value)
+})
+
+function sortBreed(letter = "a") {
+    const breedList = breedsUl.querySelectorAll('li')
+    breedList.forEach(breed => {
+        if (breed.innerText.startsWith(letter)) {
+            breed.style.display = ""
+        } else {
+            breed.style.display = "none"
+        }    
     })
+}
+
+function turnRed() {
+    let notRed = false
+    breedsUl.querySelectorAll('li').forEach(li => {    
+        li.addEventListener('click', function(event) {
+            (notRed == false)  ?
+                event.target.style.color = 'red' : event.target.style.color = 'black' 
+            notRed = !notRed
+        })
+    });
+}
